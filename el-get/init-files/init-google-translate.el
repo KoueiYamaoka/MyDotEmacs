231e146320d1536aeae3a13233cb8e7de1c32683
KoueiYamaoka <kouei525@gmail.com>
2020-01-17 23:52:44 +0900

    Add missed init file

--- el-get/init-files/init-google-translate.el
+++ el-get/init-files/init-google-translate.el
@@ -27,5 +27,4 @@
 (global-set-key "\C-xt" 'google-translate-enja-or-jaen)
 (global-set-key "\C-x\C-t" 'google-translate-query-translate)
 
-
 ;;; init-google-translate.el ends here
---
+++ el-get/init-files/init-undohist.el
@@ -0,0 +1,8 @@
+;;; undohist.el -- persistent undo history
+;;; commentary:
+;;; code:
+
+(require 'undohist)
+(undohist-initialize)
+
+;;; init-undohist.el ends here
