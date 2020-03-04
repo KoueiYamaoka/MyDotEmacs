d0e9c32b57209551fa5c39ff4fe791e012916eb9
KoueiYamaoka <kouei525@gmail.com>
2020-03-04 20:35:49 +0900

    Remove api-key

--- el-get/init-files/init-wakatime-mode.el
+++ el-get/init-files/init-wakatime-mode.el
@@ -3,7 +3,5 @@
 ;;; Code:
 
 (global-wakatime-mode)
-(custom-set-variables '(wakatime-api-key "e00fa26e-3f14-4351-832e-baa4a40f8fb3"))
-;; (custom-set-variables '(wakatime-cli-path "$ which wakatime"))
 
 ;;; init-wakatime-mode.el ends here
