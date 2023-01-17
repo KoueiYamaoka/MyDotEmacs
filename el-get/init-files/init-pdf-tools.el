5f99c836c50c86697300a7d7dab9608459fdc3f3
Kouei Yamaoka <kouei525@gmail.com>
2023-01-17 16:46:28 +0900

    Make options better

--- el-get/init-files/init-pdf-tools.el
+++ el-get/init-files/init-pdf-tools.el
@@ -2,8 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
+(pdf-loader-install)
 (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode))
-(add-hook 'pdf-view-mode-hook 'pdf-tools-install)
 (add-hook 'pdf-view-mode-hook 'pdf-tools-enable-minor-modes)
+(add-hook 'pdf-view-mode-hook 'auto-revert-mode)
 
 ;;; init-pdf-tools.el ends here
