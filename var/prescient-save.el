848b578c935e3b1150c09aac3d481e42dbb083c3
Kouei Yamaoka <kyamaoka298@gmail.com>
2026-03-09 18:01:24 +0900

    update

--- var/prescient-save.el
+++
@@ -1,2 +0,0 @@
-;;; -*- lexical-binding: t; -*-
-(funcall prescient-cache-callback :version '5 :history '#s(hash-table test equal data ("kill-line" 1 "kill-emacs" 0)) :frequency '#s(hash-table test equal data ("kill-line" 0.994009 "kill-emacs" 0.997)) :serial-number '2)
